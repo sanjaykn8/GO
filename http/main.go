d30431bc6d243aac25aa9799ecedd79ee66e608a
sanjaykn8 <nisanir8@gmail.com>
2025-11-08 15:00:56 +0530

    http

--- http/main.go
+++ http/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func msg(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	response := string("The code works.. Fr")
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/", msg)
+	port := ":3155"
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("ok")) })
+	http.ListenAndServe(port, nil)
+}
